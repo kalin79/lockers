d765c1982b1ebcbb584325913f33f49cbec51707
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2022-06-21 13:14:54 -0500

    configuracion de GA4

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WKPMWQT');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('meta_tags')
@@ -9,29 +16,10 @@
     <link href="{{ asset('frontend/css/main.css') }}" rel="stylesheet">
     <meta name="robots" content="index, follow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    {{-- <link rel="apple-touch-icon" sizes="57x57" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/ico2.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/ico20.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/ico2.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/ico2.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/ico2.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/ico2.png">
-    <link rel="shortcut icon" href="/favicon.ico">
-    <link rel="manifest" href="/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff"> --}}
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZFL3P7"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKPMWQT"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <div id="app" class="backgroundGlobal">
--- resources/views/page/frontend/inicio.blade.php
+++ resources/views/page/frontend/inicio.blade.php
@@ -6,7 +6,7 @@
     <!-- Schema.org markup for Google+ -->
     <meta itemprop="name" content="Lockers + Plus" >
     <meta itemprop="description" content=" Optimice su espacio y ofrezca seguridad para  documentos, artículos de trabajo u objetos personales">
-    <meta itemprop="image" content="https://locker.codegraph.pe/frontend/images/share.png">
+    <meta itemprop="image" content="https://lockersplus.com.pe/frontend/images/share.png">
     <!-- Twitter Card data -->
     <meta name="twitter:card" content="summary">
     <meta name="twitter:site" content="@publisher_handle">
@@ -14,15 +14,15 @@
     <meta name="twitter:description" content=" Optimice su espacio y ofrezca seguridad para  documentos, artículos de trabajo u objetos personales">
     <meta name="twitter:creator" content="@author_handle">
     <!-- Twitter Summary card images must be at least 120x120px  -->
-    <meta name="twitter:image" content="https://locker.codegraph.pe/frontend/images/share.png">
+    <meta name="twitter:image" content="https://lockersplus.com.pe/frontend/images/share.png">
 
     <!-- Open Graph data -->
     <meta property="og:title" content="Lockers + Plus"  />
     <meta property="og:type" content="article" />
-    <meta property="og:url" content="https://zoologicoamigable.com" />
-    <meta property="og:image" content="https://locker.codegraph.pe/frontend/images/share.png" />
+    <meta property="og:url" content="https://lockersplus.com.pe" />
+    <meta property="og:image" content="https://lockersplus.com.pe/frontend/images/share.png" />
     <meta property="og:description" content=" Optimice su espacio y ofrezca seguridad para  documentos, artículos de trabajo u objetos personales" />
-    <meta property="og:site_name" content="https://zoologicoamigable.com/, Lima Perú" />
+    <meta property="og:site_name" content="https://lockersplus.com.pe, Lima Perú" />
     <meta property="article:published_time" content="2023-09-17T05:59:00+01:00" />
     <meta property="article:modified_time" content="2023-09-16T19:08:47+01:00" />
     <meta property="article:section" content="Article Section" />
